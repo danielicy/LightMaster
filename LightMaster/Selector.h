04fce4f600683009bdfded7754e4b2e1303fdd1d
Danieli <danielicy@hotmail.com>
2017-11-13 08:31:27 +0200

    added selector

---
+++ LightMaster/Selector.h
@@ -0,0 +1,43 @@
+// Selector.h
+
+#ifndef _SELECTOR_h
+#define _SELECTOR_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+#ifndef Selector_h
+#define Selector_h
+
+#include "Arduino.h"
+
+class Selector
+{
+public:
+	Selector(int pin);
+	void Select(int i);
+private:
+	void White();
+	void Blue();
+	void BlueWhite();
+	void Purple();
+	void PurpleWhite();
+	void Green();
+	void GreenWhite();
+	void Red();
+	void RedWhite();
+	void Yellow();
+	void YellowGreen();
+	void PurpleBlue();
+
+
+
+};
+
+#endif
+
