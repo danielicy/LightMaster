e4960d09b3e3873125db55cdb5fefb7e166eefd3
danieli <danieli@razorgrip.com>
2017-11-19 14:46:17 +0200

    added LM_System & Selector

---
+++ LightMaster/LightMaster.ino
@@ -0,0 +1,20 @@
+#include "LM_System.h"
+#include "Selector.h"
+ 
+
+
+
+LMSystem *m_lMSystem = new LMSystem();
+
+void setup() {
+	  
+}
+
+void loop() {
+	m_lMSystem->DoWork();
+}
+
+ 
+ 
+
+
---
+++ LightMaster/Selector.h
@@ -0,0 +1,46 @@
+// Selector.h
+
+#ifndef _SELECTOR_h
+#define _SELECTOR_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+#ifndef Selector_h
+#define Selector_h
+
+#include "Arduino.h"
+
+class Selector
+{
+public:
+	Selector(int pin);
+	Selector();
+	~Selector();
+	void Select(int i);
+private:
+	void Fade();
+	/*void White();
+	void Blue();
+	void BlueWhite();
+	void Purple();
+	void PurpleWhite();
+	void Green();
+	void GreenWhite();
+	void Red();
+	void RedWhite();
+	void Yellow();
+	void YellowGreen();
+	void PurpleBlue();*/
+
+
+
+};
+
+#endif
+
