19a03ee1a3c5be5dcec7e5fe770472204b679abd
Danieli <danielicy@hotmail.com>
2017-12-04 20:43:00 +0200

    DummySystem -> ActionManager

--- LightMaster/Dummy/DummySystem.cpp
+++ LightMaster/Dummy/DummySystem.cpp
@@ -1,9 +1,18 @@
 #include "stdafx.h"
 #include "DummySystem.h"
 
+#include "ActionManager.h"
+//#include "ColorManager.h"
+//#include "Selector.h"
+
+
+
 
 DummySystem::DummySystem()
 {
+	m_ActionManager = new ActionManager(m_pinBuffer);
+	//m_colorManager = new ColorManager();
+	//m_selector = new Selector(m_colorManager, m_ActionManager);
 }
 
 
--- LightMaster/Dummy/DummySystem.h
+++ LightMaster/Dummy/DummySystem.h
@@ -1,11 +1,22 @@
 #pragma once
 #include "ActionManager.h"
+#include "ColorManager.h"
+#include "Selector.h"
+
+
+//#define LMSYSIMP __declspec(dllimport)
+
+#define PINCNT 20
 
 class DummySystem
 {
 public:
 	DummySystem();
 	~DummySystem();
+	Selector* m_selector;
 	ActionManager* m_ActionManager;
+	ColorManager* m_colorManager;
+private:
+	int m_pinBuffer[PINCNT][2];
 };
 
